4bdf7e2e71b85d883ca5dac3754a4d29002b0a77
Tamer Shlash <mr.tamershlash@gmail.com>
2016-12-13 05:19:53 +0800

    absolute path for youtube-dl

--- download.js
+++ download.js
@@ -1,5 +1,5 @@
 var exec   = require('child_process').execSync;
 
 module.exports = function(video_id) {
-  exec("youtube-dl -x --audio-format wav -o audio.wav -- " + video_id);
+  exec("/usr/local/bin/youtube-dl -x --audio-format wav -o audio.wav -- " + video_id);
 }
